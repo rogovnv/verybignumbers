15992500efcd2b11ef921d8e67178dbff5c71429
rogovnv <midtower@yandex.ru>
2022-04-03 19:50:06 +0500

    src folder

---
+++ src/storage-0.3.erl
@@ -0,0 +1,54 @@
+%% @author midtower@yandex.ru
+%% @doc storage for numbers, it is local for each string
+
+
+-module(storage).
+-behaviour(gen_server).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start/0, getnum/2, add/2, stop/1]).
+
+start() ->
+  gen_server:start_link(?MODULE, [], []).
+
+getnum(Pid, N) ->
+  gen_server:call(Pid, {get, N}).%%control in external data list length
+
+add(Pid, N) ->
+  gen_server:cast(Pid, {add, N}).
+
+stop(Pid) ->
+  gen_server:cast(Pid, stop).
+
+init(_) ->%% it returns list of 26 not initialized variables and empty list for numbers
+  process_flag(trap_exit, true),
+  ListVar=ets:new(listvar,[]),
+  {ok, {ListVar, 0}}.
+
+handle_call({get, N}, _From, {ListVar, Len}) ->
+  [F]=ets:lookup(ListVar, N),
+  {reply, F, {ListVar, Len}};
+
+handle_call({add, Num}, _From, {ListVar, Len}) ->
+  Len2=Len+1,
+  ListVar2=ets:insert(ListVar, {Len2, Num}),
+  {reply, Len2, {Len2, ListVar2}};
+
+handle_call(Other, _From, {ListVar, L}) ->
+  {reply, {badarg, Other}, {ListVar, L}}.
+
+handle_cast(stop, State) ->
+  {stop, normal, State}.
+
+handle_info(_, State) ->
+  {noreply, State}.
+
+terminate(_, {ListVar, _}) ->
+  ets:delete(ListVar),
+  ok.
+
+code_change(_, State, _) ->
+  {ok, State}.
+
+
+
---
+++ src/var-0.3.erl
@@ -0,0 +1,76 @@
+%% @author midtower@yandex.ru
+%% @doc storage for variables and strings, it is global for each task
+
+
+-module(var).
+
+-behaviour(gen_server).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start/0, stop/1, setvar/3, getvar/2, get_task_str/2, set_task_str/2]).
+
+start() ->
+  gen_server:start_link(?MODULE, [], []).
+
+stop(Pid) ->
+  gen_server:cast(Pid, stop).
+
+init(_) ->
+  ListStr=ets:new(taskstr, []),
+  M=mapgen(),
+  {ok, {M, ListStr, 0}}.
+
+mapgen() ->
+  M=#{},
+  gen($A, M).
+
+gen(Alf, M) ->
+  M1=M#{Alf => 0},
+  gen(Alf+1, M1);
+
+gen($Y, M) ->
+  M#{$Z => 0}.
+
+get_task_str(Pid, N) ->
+  gen_server:call(Pid, {gets, N}).
+
+set_task_str(Pid, List) ->
+  gen_server:call(Pid, {sets, List}).
+
+setvar(Pid, V, N) ->
+  gen_server:cast(Pid, {set, V, N}).
+
+getvar(Pid, V) ->
+  gen_server:call(Pid, {get, V}).
+
+handle_call({gets, Num}, _From, {M, L, N}) ->
+  [F]=ets:lookup(L, Num),
+  {reply, F, {M, L, N}};
+
+handle_call({sets, List}, _From, {M, L, N}) ->
+  N2=N+1,
+  L2=ets:insert(L, {N2, List}),
+  {reply, N2, {M, L2, N2}};
+
+handle_call({get, V}, _From, {M, L, N}) ->
+  #{V := Out}=M,
+  {reply, Out, {M, L, N}}.
+
+handle_cast({set, V, N}, {M, L, N}) ->
+  {noreply, {M#{V := N}, L, N}};
+
+handle_cast(stop, {M, L, N}) ->
+  {stop, normal, {M, L, N}}.
+
+terminate(_, {_, L, _}) ->
+	ets:delete(L),
+  ok.
+
+handle_info(_, State) ->
+  {noreply, State}.
+
+code_change(_, {M, L, N}, _) ->
+  {ok, {M, L, N}}.
+
+
+
