2220c01507ff0289605611896c7fd01df48b025c
rogovnv <midtower@yandex.ru>
2022-05-16 22:42:30 +0500

    working var&preproc

--- src/preproc.erl
+++ src/preproc.erl
@@ -1,5 +1,5 @@
 %% @author midtower@yandex.ru
-%% @doc @todo Add description to.
+%% @doc preprocessor
 
 -module(preproc).
 
@@ -22,22 +22,35 @@ start({VarPid, StringId}) ->
 init({VarPid, StringId}) ->
   process_flag(trap_exit, true),
   Str=gen_server:call(VarPid, {gets, StringId}),
-
-  Vars=re:run(Str, "[A-Z|_]+", [global]),
-  Varv=[{X, {v,lists:sublist(Str, X, Y)}}|| {X, Y} <- Vars],
+  {match, Vara}=re:run(Str, "[A-Z|_]+", [global]),
+  Varb=lists:flatten(Vara),
+  Varv=[{X+1, {v,lists:sublist(Str, X+1, Y)}}|| {X, Y} <- Varb],
   Str2=replacevar(Str, Varv),
-
-  NegN=re:run(Str2, "=-[0-9]+(.[0-9])*+(e[0-9]+)*|(-[0-9]+(.[0-9])*+(e[0-9]+)*", [global]),
-  NegNv=[{X, {n, lists:sublist(Str2, X+1, Y)}}|| {X, Y} <- NegN],
-  Str3=replacevar(Str2, NegNv),
-
-  PosN=re:run(Str3, "[0-9]+(.[0-9])*+(e[0-9]+)*", [global]),
-  PosNv=[{X, {n, lists:sublist(Str3, X, Y)}}|| {X, Y} <- PosN],
-  Str4=replacevar(Str3, PosNv),
-
-  Ins=lists:flatten(lists:sort(fun({A, _C}, {B, _D}) -> B>A end, Varv++NegNv++PosNv)),
+  N=re:run(Str2, "[-+*/=(]-[0-9]+.[0-9]+e[0-9]+|[-+*/=(]-[0-9]+.[0-9]+|[-+*/=(]-[0-9]+", [global]),
+  case N of
+    {match, NegN} ->
+      E=lists:flatten(NegN),
+      Negv=[{X+2, {n,lists:sublist(Str2, X+2, Y-1)}}|| {X, Y} <- E],
+      Str3=replacevar(Str2, Negv);
+    _ ->
+      Str3=Str2,
+      Negv=[]
+  end,
+  R=re:run(Str3, "[0-9]+.[0-9]+e[0-9]+|[0-9]+.[0-9]+|[0-9]+", [global]),
+  Str5=Str3,
+  case R of
+    {match, Posa} ->
+      Posb=lists:flatten(Posa),
+      %%Str3 = Str3,
+      Posv=[{X+1, {n, lists:sublist(Str5, X+1, Y)}}||{X, Y} <- Posb],
+      Str4=replacevar(Str5, Posv);
+    _ ->
+      %%Str5 = Str3, beeaach
+      Posv=[],
+      Str4=Str5
+  end,
+  Ins=lists:flatten(lists:sort(fun({A, _C}, {B, _D}) -> B>A end, Varv++Negv++Posv)),
   Insert=[F||{_, F}<-Ins],
-
   {ok, undef, {VarPid, Str4, Insert, [], 0, StringId}, [{next_event, internal, []}]}.
 %% State is: VarPid curent Pid on global data for task
 %% Str4&Insert original task string,
@@ -48,115 +61,117 @@ init({VarPid, StringId}) ->
 %% Output: wrong_begin, wrong_bracket_number, wrong_lbracket, wrong_lvar, bad_number,
 %% wrong_oper, wrong_rbacket, wrong_rvar
 
-undef(internal, _, {VarPid, Str, Ins, [], LRBr, SId}) ->
+undef(internal, _Data, {VarPid, Str, Ins, [], LRBr, SId}) ->
   [{A, B}|Ins2]=Ins,
-  [[Z, X]|T]=Str,
-  if
-    [Z, X]==["v="] and A=="v" ->
-      gen_server:cast(VarPid, {initv, B}),
-      [P|Str2]=T,
-      {next_state, lbr, {VarPid, Str2, Ins2, [{A, B}, "="], LRBr, SId}, [{next_event, internal, P}]};
+  [Q,Z|T2]=Str,
+  Bool=A==v,
+  Booool=[Q, Z]=="v=",
+  R=Bool and Booool,
+  case R of
     true ->
+      gen_server:cast(VarPid, {initv, B}),
+      [P|Str2]=T2,
+      {next_state, lbr, {VarPid, Str2, Ins2, Q, LRBr, SId}, [{next_event, internal, P}]};%% eq symbol fired
+    _ ->
       MP=gen_server:call(VarPid, master),
-      gen_server:reply(MP, {SId, wrong_begin}),
+      gen_server:cast(MP, {SId, wrong_begin}),
       keep_state_and_data
   end.
 
-%% end handling in left state
-lbr(internal, _Data, {VarPid, Str, _Ins, Aout, LRBr, SId}) when Str == [] ->
-  MP=gen_server:call(VarPid, master),
-  Answer= case LRBr of
-            0 -> {SId, Aout};
-            true -> {SId, wrong_bracket_number}
-          end,
-  gen_server:reply(MP, {SId, Answer}),
-  keep_state_and_data;
-
 %% left bracket, keepstate
-lbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $( ->
+lbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $(->
   if
     LRBr >= 0 ->
-      [H|T]=Str,
-      {keep_state, {VarPid, T, Aout++$(, Ins, LRBr+1, SId}, [{next_event, internal, H}]};
+      [H, T]=Str,
+      {keep_state, {VarPid, T, Ins, [Aout,"("], LRBr+1, SId}, [{next_event, internal, H}]};
     true ->
       MP=gen_server:call(VarPid, master),
-      gen_server:reply(MP, {SId, wrong_lbracket}),
+      gen_server:cast(MP, {SId, wrong_bracket}),
       keep_state_and_data
   end;
 
 %% var or number, switch to oper
-lbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $v ->
+lbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $v->
   [H|T]=Str,
-  [HI|TI]=Ins,
-  case HI of
+  [{A, B}|TI]=Ins,
+  case {A, B} of
     {n, Num} ->
       HIns={n, perform_num(VarPid, Num)},
-      {next_state, oper, {VarPid, T, TI, Aout++HIns, LRBr, SId}, [{next_event, internal, H}]};
+      {next_state, oper, {VarPid, T, TI, [Aout,HIns], LRBr, SId}, [{next_event, internal, H}]};
     {v, Vname} -> %% var
       Bool=gen_server:call(VarPid, {is_existv, Vname}),
       case Bool of
         badkey ->
           MP=gen_server:call(VarPid, master),
-          gen_server:reply(MP, {SId, wrong_lvar}),
+          gen_server:cast(MP, {SId, wrong_lvar}),
           keep_state_and_data;
-        true -> {next_state, oper, {VarPid, T, TI, Aout++HI, LRBr, SId}, [{next_event, internal, H}]}
+        _ ->
+          {next_state, oper, {VarPid, T, TI, [Aout,{A, B}], LRBr, SId}, [{next_event, internal, H}]}
       end
   end;
 
 %% wrong symbol
-lbr(internal, _Data, {VarPid, _Str, _Aout , _Ins, _LRBr, SId}) ->
+lbr(internal, _Data, {VarPid, _Str, _Ins , _Aout, _LRBr, SId}) ->
   MP=gen_server:call(VarPid, master),
-  gen_server:reply(MP, {SId, bad_number}),
+  gen_server:cast(MP, {SId, bad_number}),
   keep_state_and_data.
 
-rbr(internal, _Data, {VarPid, Str, _Aout , _Ins, _LRBr, SId}) when Str==[] ->
-  MP=gen_server:call(VarPid, master),
-  gen_server:reply(MP, {SId, wrong_oper}),
-  keep_state_and_data;
-
 %% transition
-rbr(internal, Data, {VarPid, Str, Aout, Ins, LRBr, SId}) when Data == $( ->
-  {next_state, lbr, {VarPid, Str, Aout, Ins, LRBr, SId}, [{next_event, internal, Data}]};
+rbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $( ->
+  [H|T]=Str,
+  {next_state, lbr, {VarPid, T, Ins, [Aout,Data], LRBr+1, SId}, [{next_event, internal, H}]};
 
 rbr(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data ==$v ->
   [H|T]=Str,
-  [HI|TI]=Ins,
-  case HI of
+  [{A, B}|TI]=Ins,
+  case {A, B} of
     {n, Num} ->
       HIns={n, perform_num(VarPid, Num)},
-      {next_state, oper, {VarPid, T, TI, Aout++HIns, LRBr, SId}, [{next_event, internal, H}]};
+      {next_state, oper, {VarPid, T, TI, [Aout,HIns], LRBr, SId}, [{next_event, internal, H}]};
     {v, Vname} -> %% var
       Bool=gen_server:call(VarPid, {is_existv, Vname}),
       case Bool of
         badkey ->
           MP=gen_server:call(VarPid, master),
-          gen_server:reply(MP, {SId, wrong_rvar}),
+          gen_server:cast(MP, {SId, wrong_rvar}),
           keep_state_and_data;
-        true -> {next_state, oper, {VarPid, T, TI, Aout++HI, LRBr, SId}, [{next_event, internal, H}]}
+        _ ->
+          {next_state, oper, {VarPid, T, TI, [Aout,{A, B}], LRBr, SId}, [{next_event, internal, H}]}
       end
   end.
 
-oper(internal, _Data, {VarPid, Str, _Ins, Aout, LRBr, SId})  when Str == [] ->
+oper(internal, Data, {VarPid, [], _Ins, Aout, LRBr, SId})  when Data == " "  ->
   MP=gen_server:call(VarPid, master),
-  Answer= case LRBr of
-            0 -> {SId, Aout};
+  Answer= if
+            LRBr == 0 -> {SId, Aout};
             true -> {SId, wrong_bracket_number}
           end,
-  gen_server:reply(MP, {SId, Answer}),
+  gen_server:cast(MP, Answer),
   keep_state_and_data;
 
-oper(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) ->
+oper(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data ==$+ orelse Data ==$- orelse Data ==$* orelse Data ==$/ ->
   [H|T]=Str,
-  if
-    Data ==$+ orelse Data ==$- orelse Data ==$* orelse Data ==$/ ->
-      {next_state, rbr, {VarPid, T, Ins, Aout++Data, LRBr, SId}, [{next_event, internal, H}]};
-    Data ==$) ->
-      {keep_state, {VarPid, T, Ins, Aout++Data, LRBr-1, SId}, [{next_event, internal, H}]};
+  {next_state, rbr, {VarPid, T, Ins, [Aout,Data], LRBr, SId}, [{next_event, internal, H}]};
+
+oper(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $) ->
+  Bool=Str =:= [],
+  case Bool of
     true ->
-      MP=gen_server:call(VarPid, master),
-      gen_server:reply(MP, {SId, wrong_oper}),
-      keep_state_and_data
-  end.
+      H=" ",
+      T=[];
+    false ->
+      [H|T]=Str
+  end,
+  {keep_state, {VarPid, T, Ins, [Aout,Data], LRBr-1, SId}, [{next_event, internal, H}]};
+
+oper(internal, Data, {VarPid, Str, Ins, Aout, LRBr, SId}) when Data == $( ->
+  [H|T]=Str,
+  {next_state, lbr, {VarPid, T, Ins, [Aout,Data], LRBr+1, SId}, [{next_event, internal, H}]};
+
+oper(internal, _Data, {VarPid, _Ins, _Str, _Aout, _LRBr, SId}) ->
+  MP=gen_server:call(VarPid, master),
+  gen_server:cast(MP, {SId, wrong_oper}),
+  keep_state_and_data.
 
 stop(Pid) ->
   gen_statem:stop(Pid).
@@ -171,7 +186,7 @@ perform_num(VarPid, Num) ->
     1 ->
       list_to_integer(lists:flatten(Num++lists:duplicate(Range, $0)));
     2 ->
-      F=lists:nth(Split, 2),
+      F=lists:nth(2, Split),
       L=length(F),
       D= if
            L > Range ->
@@ -179,31 +194,30 @@ perform_num(VarPid, Num) ->
            true ->
              F++lists:duplicate(Range-L, $0)
          end,
-      list_to_integer(lists:flatten(lists:nth(Split, 1)++D));
+      list_to_integer(lists:flatten(lists:nth(1, Split)++D));
     3 ->%% calculate and keep in mind dot pos
-      F=lists:nth(Split, 2),%% numbers after dot
-      L=length(lists:nth(Split, 1)),
-      R=list_to_integer(lists:nth(Split, 3)),%% exp
-      Med=lists:nth(Split, 1)++F++lists:duplicate(Range, $0),
+      F=lists:nth(2, Split),%% numbers after dot
+      L=length(lists:nth(1,Split)),
+      R=list_to_integer(lists:nth(3, Split)),%% exp
+      Med=lists:nth(1, Split)++F++lists:duplicate(Range, $0),
       D=L+R,
       Dot= if
              D<0 ->
                Med2=lists:duplicate(-D, $0)++Med,
                0;
              true ->
-               Med2=Med,
+               Med2=Med++lists:duplicate(R, $0),
                D
            end,
-      list_to_integer(lists:sublist(lists:flatten(Med2), 0, Dot+Range))
+      list_to_integer(lists:sublist(lists:flatten(Med2), 1, Dot+Range))
   end.
 
 replacevar(Str, []) ->
   Str;
 replacevar(Str, Vars) ->
-  {_, Z}=hd(Vars),
+  {_,{_, Z}}=hd(Vars),
   F=re:replace(Str, Z, "v", [{return, list}]),
   replacevar(F, tl(Vars)).
-
 
 
 
--- src/var.erl
+++ src/var.erl
@@ -11,14 +11,14 @@
 -export([initv/2, is_existv/2]).
 
 start({Prange, MasterPid}) ->
-  gen_server:start(?MODULE, [{Prange, MasterPid}], []).
+  gen_server:start(?MODULE, {Prange, MasterPid}, []).
 
 stop(Pid) ->
-  gen_server:cast(Pid, stop).
+  gen_server:stop(Pid).
 
 init({Prange, MasterPid}) ->
-  ListStr=ets:new(taskstr, []),
-  M=#{},
+  ListStr=#{},
+  M=#{0=>"4"},
   {ok, {M, ListStr, 0, Prange, MasterPid}}.
 %% M variables, L numbers,N 0 numbers count, Pr precision range, MP Pid of outer process to return output
 
@@ -50,7 +50,7 @@ handle_call({is_existv, Vname}, _From, {M, L, N, Pr, MP}) ->
   P=map_get(Vname, M),
   Out = case P of
     {badkey, _} -> badkey;
-    _ -> P
+    _ -> true
   end,
   {reply, Out, {M, L, N, Pr, MP}};
 
@@ -61,33 +61,24 @@ handle_call(range, _From, {M, L, N, Pr, MP}) ->
   {reply, Pr, {M, L, N, Pr, MP}};
 
 handle_call({gets, Num}, _From, {M, L, N, Pr, MP}) ->
-  [F]=ets:lookup(L, Num),
+  #{Num := F}=L,
   {reply, F, {M, L, N, Pr, MP}};
 
 handle_call({sets, List}, _From, {M, L, N, Pr, MP}) ->
   N2=N+1,
-  L2=ets:insert(L, {N2, List}),
-  {reply, N2, {M, L2, N2, Pr, MP}};
+  {reply, N2, {M, L#{N2 => List}, N2, Pr, MP}};
 
 handle_call({get, V}, _From, {M, L, N, Pr, MP}) ->
   #{V := Out}=M,
   {reply, Out, {M, L, N, Pr, MP}}.
 
 handle_cast({initv, Vname}, {M, L, N, Pr, MP}) ->
-  P=map_get(Vname, M),
-  case P of
-    {badkey, _} -> {noreply, {M#{Vname => 0}, L, N, Pr, MP}};
-    _ -> {noreply, {M, L, N, Pr, MP}}
-  end;
+   {noreply, {M#{Vname => 0}, L, N, Pr, MP}};
 
 handle_cast({set, V, N}, {M, L, N, Pr, MP}) ->
-  {noreply, {M#{V := N}, L, N, Pr, MP}};
+  {noreply, {M#{V := N}, L, N, Pr, MP}}.
 
-handle_cast(stop, {M, L, N, Pr, MP}) ->
-  {stop, normal, {M, L, N, Pr, MP}}.
-
-terminate(_, {_, L, _}) ->
-	ets:delete(L),
+terminate(_Reason, _Data) ->
   ok.
 
 handle_info(_, State) ->
